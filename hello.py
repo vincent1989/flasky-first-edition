89fcf09b0432b73e3184760e9a039a12749c90a8
卢文昌 <lwc@jiagouyun.com>
2017-10-25 11:11:07 +0800

    练习

--- hello.py
+++ hello.py
@@ -1,11 +1,26 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import abort
+from flask.ext.script import Manager
 
+app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello %s</h1>'.format(name)
+
+#
+# @app.route('/user/<id>')
+# def get_user(id):
+#     user = load_user(id)
+#     if not user:
+#         abort(404)
+#     return '<h1>Hello %s</h1>'.format(name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()
