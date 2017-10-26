2646b4af83d7b31ad70388b432410743730e08db
卢文昌 <lwc@jiagouyun.com>
2017-10-26 14:46:55 +0800

    42页 前三章结束

--- hello.py
+++ hello.py
@@ -48,6 +48,9 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name!=form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index', current_time=datetime.utcnow()))
     return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
