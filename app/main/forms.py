be3656f44f436038718ff87c8f603258c87db638
卢文昌 <lwc@jiagouyun.com>
2017-11-03 17:44:50 +0800

    保存10.3节

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,10 +9,17 @@ from . import main
 # from flask.ext.wtf import Form
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField, BooleanField
 # 下面这一行的包处于将废弃的状态
 # from wtforms.validators import Required
 from wtforms.validators import DataRequired
+from wtforms.validators import Length
+from wtforms.validators import Email
+from wtforms.validators import Regexp
+from wtforms.validators import ValidationError
+
+from ..models import Role
+from ..models import User
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -21,3 +28,50 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     # 注意，验证函数 DataRequired() 可以验证数据不为空
     submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    '''用户级别的-资料编辑表单'''
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('保存')
+
+
+class EditProfileAdminForm(FlaskForm):
+    '''管理员级别-资料编辑表单'''
+
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp(
+        '^[a-zA-Z][a-zA-Z0-9_.]*$',
+        0,
+        '用户名必须已字母开头，只能包含大小写字母，数字，下划线或点号'
+    )])
+    confirmed = BooleanField('Confirmed')
+    # SelectField 实例必须在 choices 属性中设置各选项，选项必须时一个元组组成的列表，
+    # 各元组必须包含两个元素：选项的标识符 和 显示在控件中的文本字符串
+    # 本例中 元组中的标识符是 角色ID，角色名
+    # 另外 coerce=int 是为了保证SelectField的data 值为整数
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('保存')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        '''验证有效性'''
+        if (field.data != self.user.email
+            and User.query.filter_by(email=field.data).first()):
+            raise ValidationError('邮件已被注册！')
+
+    def validate_username(self, field):
+        '''验证用户名有效性'''
+        if (field.data != self.user.username
+            and User.query.filter_by(username=field.data).first()):
+            raise ValidationError('用户名已被注册！')
+
--- app/main/views.py
+++ app/main/views.py
@@ -8,13 +8,22 @@ from flask import session
 from flask import redirect
 from flask import url_for
 from flask import abort
+from flask import flash
+from flask_login import login_required
+from flask_login import current_user
 
 
 
 from . import main
 from .forms import NameForm
+from .forms import EditProfileForm
+from .forms import EditProfileAdminForm
+
 from .. import db
 from ..models import User
+from ..models import Role
+from ..decorators import admin_required
+
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -40,7 +49,57 @@ def index():
 
 @main.route('/user/<username>', methods=['GET', 'POST'])
 def user(username):
+    '''用户资料页面'''
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    render_template('user.html', user=user)
+    return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    '''用户级别的-资料编辑'''
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        # 如果表单验证通过则，进行更新操作
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('您的资料已更新！')
+        return redirect(url_for('.user', username=current_user.username))
+
+    form.name.data = current_user.name
+    form.location  = current_user.location
+    form.about_me  = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    '''管理员级别-资料编辑'''
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email    = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role     = Role.query.get(form.role.data)
+        user.name     = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('当前用户配置已更新完毕！')
+        return redirect(url_for('.user', username=user.username))
+
+    form.email.data     = user.email
+    form.username.data  = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data      = user.role.id
+    form.name.data      = user.name
+    form.location.data  = user.location
+    form.about_me.data  = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
