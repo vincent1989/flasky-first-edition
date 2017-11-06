3b1d25d7e7836a65b6f2790f0d7808fe8b76b46d
卢文昌 <lwc@jiagouyun.com>
2017-11-06 16:18:17 +0800

    微调修复图片不显示问题，即更该https为http

--- app/models.py
+++ app/models.py
@@ -238,9 +238,9 @@ class User(UserMixin, db.Model):
 
     def gravatar(self, size=100, default='identicon', rating='g'):
         if request.is_secure:
-            url = 'https://secure.gravatar.com/avatar'
+            url = 'http://secure.gravatar.com/avatar'
         else:
-            url = 'https://www.gravatar.com/avatar'
+            url = 'http://www.gravatar.com/avatar'
 
         hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
